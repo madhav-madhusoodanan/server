465589db4a13ae4bba788d1387bc58e027d74223
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-08-04 20:01:08 +0400

    successfully read some random chars

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
 fn main() { 
     let listener = match TcpListener::bind("127.0.0.1:8080") {
@@ -8,9 +10,16 @@ fn main() {
             panic!("{}", e);
         },
     };
+
+    let handle = |mut stream: TcpStream| {
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        println!("{}", String::from_utf8_lossy(&buffer[..]));
+    };
     
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection sett!");
+        handle(stream)
     }
 }
+
