2f331df8f92f8eeddc01d285c3abd4c576d69100
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-08-07 01:28:50 +0400

    returned html successfully

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::net::TcpListener;
-use std::net::TcpStream;
+use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
+use std::fs;
 
 fn main() { 
     let listener = match TcpListener::bind("127.0.0.1:8080") {
@@ -14,7 +14,15 @@ fn main() {
     let handle = |mut stream: TcpStream| {
         let mut buffer = [0; 1024];
         stream.read(&mut buffer).unwrap();
-        println!("{}", String::from_utf8_lossy(&buffer[..]));
+
+	let contents = fs::read_to_string("rust.html").unwrap();
+	let response = format!(
+				"HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", 
+				contents.len(),
+				contents
+			);
+        stream.write(response.as_bytes()).unwrap();
+	stream.flush().unwrap();
     };
     
     for stream in listener.incoming() {
